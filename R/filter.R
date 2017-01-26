de8d42a5a21219bab1f0ddc463e5cca41c4d43ee
farhanmubasher <farhanmubasher308@gmail.com>
2017-01-26 19:34:30 +0500

    Update filter.R

--- R/filter.R
+++ R/filter.R
@@ -1,44 +1,48 @@
 
-tamahagane.apply.filter<- function(dataset, attribute, filterType="", object="")
+tamahagane.apply.filter<- function(dataset, attribute, filterType="", object)
   {
+  converted.dataset<- fromJSON(dataset)
 
-    if(filterType=="" || object=="")
+  converted.attribute<- converted.dataset[,attribute]
+
+
+    if(filterType=="")
   {
       warning("Please specify your filter type or Object !!")
   }
   else{
 
     if(filterType=="EQUAL"){
-      return( subset(dataset, attribute == object ))
+      return( subset(converted.dataset, converted.attribute == object ))
     }
     else if (filterType== "NOTEQUAL"){
 
-     return( subset(dataset, attribute != object ))
+     return( subset(converted.dataset, converted.attribute != object ))
     }
     else if(filterType== "ISIN"){
 
-      return( subset(dataset, attribute %in% object ))
+      return( subset(converted.dataset, converted.attribute %in% object ))
 
     }
     else if(filterType== "ISNOTIN")
     {
-      return( subset(dataset, attribute != object ))
+      return( subset(converted.dataset, converted.attribute != object ))
 
     }
     else if(filterType=="STARTWITH"){
-      return( subset(dataset, attribute == object ))
+      return( subset(converted.dataset, converted.attribute == object ))
     }
      else if (filterType== "ENDWITH"){
 
-      return( subset(dataset, attribute == object ))
+      return( subset(converted.dataset, converted.attribute == object ))
     }
     else if (filterType== "MATCHES"){
 
-      return( subset(dataset, attribute == object ))
+      return( subset(converted.dataset, converted.attribute == object ))
     }
    else if (filterType== "NOTMATCHES"){
 
-      return( subset(dataset, attribute != object ))
+      return( subset(converted.dataset, converted.attribute != object ))
     }
   }
 
