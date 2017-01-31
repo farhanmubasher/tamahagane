3b39dd456a274843f77a10a85b6b4954e39c45e3
farhanmubasher <farhanmubasher308@gmail.com>
2017-01-31 19:18:07 +0500

    Update filter.R

--- R/filter.R
+++ R/filter.R
@@ -1,7 +1,10 @@
 
-tamahagane.apply.filter<- function(dataset, attribute, filterType="", object)
+tamahagane.apply.filter<- function(dataset, columName, filterType="", object)
   {
   converted.dataset<- fromJSON(dataset)
+  attribute<- fromJSON(columName)
+
+  obj <- paste("^", object, sep = "")
 
   converted.attribute<- converted.dataset[,attribute]
 
@@ -13,39 +16,38 @@ tamahagane.apply.filter<- function(dataset, attribute, filterType="", object)
   else{
 
     if(filterType=="EQUAL"){
-      return( subset(converted.dataset, converted.attribute == object ))
+      return( subset(converted.dataset, converted.attribute == c(object) ))
     }
     else if (filterType== "NOTEQUAL"){
 
-     return( subset(converted.dataset, converted.attribute != object ))
+     return( subset(converted.dataset, converted.attribute != c(object)  ))
     }
     else if(filterType== "ISIN"){
 
-      return( subset(converted.dataset, converted.attribute %in% object ))
+      return( subset(converted.dataset, converted.attribute %in% c(object)  ))
 
     }
     else if(filterType== "ISNOTIN")
     {
-      return( subset(converted.dataset, converted.attribute != object ))
+      return( subset(converted.dataset, converted.attribute != c(object)  ))
 
     }
     else if(filterType=="STARTWITH"){
-      return( subset(converted.dataset, converted.attribute == object ))
+
+      return( converted.dataset[grep(obj, converted.dataset[,attribute]) ,])
     }
      else if (filterType== "ENDWITH"){
 
-      return( subset(converted.dataset, converted.attribute == object ))
+      return( subset(converted.dataset, converted.attribute == c(object)  ))
     }
     else if (filterType== "MATCHES"){
 
-      return( subset(converted.dataset, converted.attribute == object ))
+      return( subset(converted.dataset, converted.attribute == c(object)  ))
     }
    else if (filterType== "NOTMATCHES"){
 
-      return( subset(converted.dataset, converted.attribute != object ))
+      return( subset(converted.dataset, converted.attribute != c(object)  ))
     }
   }
 
 }
-
-
