21cce9edf72aed266061e2874fd5131f7e4c27ad
farhanmubasher <farhanmubasher308@gmail.com>
2017-02-09 10:38:52 +0500

    Update filter.R
    Multi Filtering

--- R/filter.R
+++ R/filter.R
@@ -1,54 +1,57 @@
-
-tamahagane.apply.filter<- function(dataset, columName, filterType="", object)
+tamahagane.apply.filter<- function(Dataset, columNames, filterType, Values)
+{
+  converted.Dataset <- fromJSON(Dataset)
+  cols<- fromJSON(columNames)
+  converted.filterType <- fromJSON(filterType)
+  converted.Values <- fromJSON(Values)
+  
+  j<-1
+  while(j<=length(converted.Values))
   {
-  converted.dataset<- fromJSON(dataset)
-  attribute<- fromJSON(columName)
-
-  obj.start <- paste("^", object, sep = "")
-  obj.end <- paste(object, "$", sep = "")
-
-  converted.attribute<- converted.dataset[,attribute]
-
-
-    if(filterType=="")
+    if(converted.filterType[j] == "STARTWITH" | converted.filterType[j] == "ENDWITH")
+    {
+      obj.start <- paste("^", converted.Values[j] , sep = "")
+     obj.end <- paste(converted.Values[j] , "$", sep = "")
+    }
+    
+    j<- j+1
+  }
+  i<- 1
+  while(i <= length(converted.Values) )
   {
-      warning("Please specify your filter type or Object !!")
+    if(converted.filterType[i]== "EQUAL"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]==converted.Values[i])
+    }
+    else if(converted.filterType[i]== "NOTEQUAL"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]!=converted.Values[i])
+    }
+    else if(converted.filterType[i]== "ISIN"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]==converted.Values[i])
+    }
+    else if(converted.filterType[i]== "ISNOTIN"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]!=converted.Values[i])
+    }
+    else if(converted.filterType[i]== "LESSTHAN"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]<converted.Values[i])
+    }
+    else if(converted.filterType[i]== "GREATERTHAN"){
+      converted.Dataset<-  subset(converted.Dataset, converted.Dataset[cols[i]]>converted.Values[i])
+    }
+    else if(converted.filterType[i]== "STARTWITH"){
+      converted.Dataset<- converted.Dataset[grep(obj.start, converted.Dataset[,cols[i]]) ,]
+    }
+    else if(converted.filterType[i]== "ENDWITH"){
+      converted.Dataset<- converted.Dataset[grep(obj.end, converted.Dataset[,cols[i]]) ,]
+    }
+    i<- i+1
+  }
+  
+  if(nrow(converted.Dataset)==0){
+      
+    return("No Records Found !!")
   }
   else{
-
-    if(filterType=="EQUAL"){
-      return( subset(converted.dataset, converted.attribute == object ))
-    }
-    else if (filterType== "NOTEQUAL"){
-
-     return( subset(converted.dataset, converted.attribute != object  ))
-    }
-    else if(filterType== "ISIN"){
-
-      return( subset(converted.dataset, converted.attribute %in% object))
-
-    }
-    else if(filterType== "ISNOTIN")
-    {
-      return( subset(converted.dataset, converted.attribute != object ))
-
-    }
-    else if(filterType=="STARTWITH"){
-
-      return( converted.dataset[grep(obj.start, converted.dataset[,attribute]) ,])
-    }
-     else if (filterType== "ENDWITH"){
-
-       return( converted.dataset[grep(obj.end, converted.dataset[,attribute]) ,])
-    }
-    else if (filterType== "LESSTHAN"){
-
-      return( subset(converted.dataset, converted.attribute < object ))
-    }
-   else if (filterType== "GREATERTHAN"){
-
-      return( subset(converted.dataset, converted.attribute > object ))
-    }
+  return(converted.Dataset)
   }
-
 }
+
