2ee98b27924a537e84877717c1c1d4d0168f484b
Farhan Mubasher <farhanmubasher308@gmail.com>
2017-02-27 14:51:50 +0500

    Update sort.R

--- R/sort.R
+++ R/sort.R
@@ -1,16 +1,49 @@
+library(jsonlite)
+library(lubridate)
+
 tamahagane.sort <- function(Dataset, columName, sortType)
 {
   Dataset <- fromJSON(Dataset)
   columName <- fromJSON(columName)
   sortType <- fromJSON(sortType)
-  if(sortType=="ASC" || sortType=="")
+  
+  check  <- tryCatch({
+    !all(is.na(as.Date(Dataset[, columName],format="%d/%m/%Y")))
+  }, error=function(e){NULL})
+
+  if(!is.null(check) && check!= FALSE)
   {
-    return (Dataset[order(Dataset[, columName]) ,])
-  }
-  else(sortType=="DESC")
-  {
-    return (Dataset[rev(order(Dataset[, columName]))  ,])
+    Dataset[columName] <- dmy(Dataset[, columName])
+    count.misssing.values  <- colSums(is.na(Dataset[columName]))
+    if(count.misssing.values >= 1)
+    {
+      return ("Date Format is not Correct")
+    }
+    else
+    {
+      
+      if(sortType=="ASC" || sortType=="")
+      {
+        return (Dataset[order(Dataset[, columName]) ,])
+      }
+      else(sortType=="DESC")
+      {
+        return (Dataset[rev(order(Dataset[, columName]))  ,])
+      }
+        
+      }
     
   }
   
+  else if (is.null(check) || check== FALSE){
+    
+    if(sortType=="ASC" || sortType=="")
+    {
+      return (Dataset[order(Dataset[, columName]) ,])
+    }
+    else(sortType=="DESC")
+    {
+      return (Dataset[rev(order(Dataset[, columName]))  ,])
+    }
+  }  
 }
