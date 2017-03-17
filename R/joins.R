f46594ea015e852b2feedefc31dbcdf70c628e46
Farhan Mubasher <farhanmubasher308@gmail.com>
2017-03-17 18:55:05 +0500

    Update joins.R

--- R/joins.R
+++ R/joins.R
@@ -1,8 +1,8 @@
 library(plyr)
-tamahagane.apply.join<- function(x,y,joinType)
+tamahagane.apply.join<- function(filePath.1,filePath.2,joinType)
 {
-  x <- fromJSON(x)
-  y <- fromJSON(y)
+  x <- fromJSON(filePath.1)
+  y <- fromJSON(filePath.2)
   joinType <- fromJSON(joinType)
   result <- join(x, y, type= joinType)
   result[is.na(result)] <- "?"
